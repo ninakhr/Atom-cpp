81f2b48db804b91ab4229a62403fd26d6fb25e73
Nina Khorobrykh <59206336+ninakhr@users.noreply.github.com>
2020-04-19 00:46:02 +0300

    Create arithmetic.cpp

---
+++ arithmetic.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a1, d, n, sum=0;
+	cin >> a1 >> d >> n;
+	for (int i = 0; i < n; i++) {
+		sum += a1 + (d*n - d);
+	}	
+	cout << sum;
+	return 0;
+}
